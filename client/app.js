443dacf19246d82848ebad9cd0d945d5c00f90cb
Adam Ziehl <ajziehl@gmail.com>
2015-11-12 14:37:23 -0600

    Start of basic structure

---
+++ client/app.js
@@ -0,0 +1,21 @@
+angular.module('myApp', [
+    'ui.router'
+  ])
+
+    .config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+
+        .state('about', {
+          url: '/about',
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl'
+        });
+});
