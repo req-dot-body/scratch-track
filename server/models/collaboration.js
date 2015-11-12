b1fc97d0bbc2f1c0a1d8a60c2715d7971069106c
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-12 16:28:28 -0600

    schemas built

---
+++ server/models/collaboration.js
@@ -0,0 +1,57 @@
+var db = require('path');
+
+var Collaboration = {};
+
+// finds by id and then calls the callback
+Collaboration.findById = function(id, cb) {
+  return db('collaboration').select('*').where({id: id}).limit(1)
+    .then(function(rows) {
+      if (!rows.length) return;
+      if (!cb) return rows[0];
+      return cb(null, rows[0]);
+    })
+    .catch(function(err) {
+      throw err;
+    });
+};
+
+// returns all collaborations for a user and calls a callback
+Collaboration.findByUser = function (user_id, cb) {
+  return db('collaborations').select('*').where({user_id: user_id})
+    .then(function(rows){
+      if (!rows.length) return;
+      if (!cb) return rows;
+      return cb(null, rows);
+    })
+    .catch(function(err)){
+      throw err;
+    })
+};
+
+//returns all collaborations for a project and calls a callback
+Collaboration.findByProject = function(project_id, cb) {
+	return db('collaborations').select('*').where({project_id: project_id})
+		.then(function(rows){
+			if (!rows.length) return;
+			if (!cb) return rows;
+			return cb(null, rows);
+		})
+		.catch(function(err){
+			throw err;
+		})
+}
+
+// creates a new collab
+Collaboration.create = function (attrs) {
+  return db('collaborations').insert(attrs).returning('id')
+    .then(function(rows) {
+      var newCollaboration = {
+        id: rows[0],
+        user_id: user_id,
+        project_id: project_id
+      };
+      return newCollaboration;
+    });
+};
+
+module.exports = Collaboration;
---
+++ server/models/user.js
@@ -0,0 +1,53 @@
+var db     = require('where ever the db lives');
+var bPromise = require('bluebird');
+var bcrypt = bPromise.promisifyAll(require('bcrypt-nodejs'));
+
+var User = {};
+
+// returns all users
+User.all = function () {
+  return db('users').select('*');
+};
+
+// finds a user by username and then calls the callback
+User.findByUsername = function(username, cb) {
+  return db('users').select('*').where({name: username}).limit(1)
+    .then(function(rows) {
+      if (!rows.length) return;
+      if (!cb) return rows[0];
+      return cb(null, rows[0]);
+    })
+    .catch(function(err) {
+      throw err;
+    });
+};
+
+// creates a new user with name, and hashed password
+User.signUp = function (attrs) {
+  return db('users').insert(attrs).returning('id')
+    .then(function(rows) {
+      var newUser = {
+        id: rows[0],
+        email: attrs.email,
+        password: attrs.password,
+        first: attrs.first,
+        last: attrs.last
+      };
+      return newUser;
+    });
+};
+
+// generates hash async
+User.generateHash = function(password) {
+  return bcrypt.genSaltAsync(8)
+    .then(function(salt) {
+      return bcrypt.hashAsync(password, salt, null);
+    });
+};
+
+// checks password async with stored password
+User.validPassword = function(password) {
+  return bcrypt.compareAsync(password, this.passHash);
+};
+
+module.exports = User;
