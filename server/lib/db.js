336bcbfec3eef12bac9ccd7b9b4ab2cf68a44ae2
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-13 09:55:11 -0600

    restructured files

---
+++ server/lib/db.js
@@ -0,0 +1,9 @@
+var bPromise = require('bluebird');
+// Read configuration file
+var config = require('../knexfile.js');
+
+// Configure knex with the correct environment configuration
+var env = process.env.NODE_ENV || 'development';
+var db = require('knex')(config[env]);
+
+module.exports = db;
