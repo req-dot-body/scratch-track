24c188e5b9c69ccc62a67dcb0b8a1c3930c2fb89
sca <smartinsantos@gmail.com>
2015-11-13 12:38:53 -0600

    added dependencies for angular-materialize

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -21,13 +21,15 @@ var sharedAngular = [
   'angular-sanitize',
   'angular-touch',
   'angular-ui-router',
+  './node_modules/angular-materialize/src/angular-materialize',
 ];
 
 router.use('/api', apiRouter);
 apiRouter.use('/users', userRouter);
 router.get('/js/app.js', browserify('./client/app.js', { transform: ngAnnotate }));
 router.get('/js/angular.js', browserify(sharedAngular));
-
+router.get('/js/jquery.js',browserify('./node_modules/jquery/dist/jquery.js'));
+// router.get('/js/materialize.js',browserify('./node_modules/materialize-css/dist/js/materialize.js'));
 
 // Catch-all router, this must be the last route
 router.get('/*', function (req, res) {
