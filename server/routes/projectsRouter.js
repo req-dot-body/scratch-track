4cf2244f7074a2029bc94437663b153cdff46577
Adam Ziehl <ajziehl@gmail.com>
2015-11-13 16:01:24 -0600

    Update project api endpoints to includes recordings, lyrics, stablatures, and notes

--- server/routes/projectsRouter.js
+++ server/routes/projectsRouter.js
@@ -2,28 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 // Get all projects that can be accessed
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.json({'success':true});
 });
 
 // Create new project
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   res.json({'success':true});
 });
 
 // Get a project by id
-router.get('/:projectId', function(req, res) {
+router.get('/:id', function (req, res) {
   res.json({'success':true});
 });
 
 // Update a project using its id
-router.put('/:projectId', function(req, res) {
+router.put('/:id', function (req, res) {
   res.json({'success':true});
 });
 
 // Delete a project using its id
-router.delete('/:projectId', function(req, res) {
+router.delete('/:id', function (req, res) {
   res.json({'success':true});
+});
+
+// Get all recordings associated with a specific project
+router.get('/:id/recordings', function (req, res) {
+
+});
+
+// Get all lyrics associated with a specific project
+router.get('/:id/lyrics', function (req, res) {
+
+});
+
+// Get all stablatures associated with a specific project
+router.get('/:id/stablature', function (req, res) {
+
+});
+
+// Get all notes associated with a specific project
+router.get('/:id/notes', function (req, res) {
+
 });
 
 module.exports = router;
