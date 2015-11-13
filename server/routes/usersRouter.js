dab63d15dfa3329f6ccc318bbb4fed702648e102
Adam Ziehl <ajziehl@gmail.com>
2015-11-13 15:51:40 -0600

    Add some placeholder responses to users api

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -13,41 +13,43 @@ router.get('/', function (req, res) {
 // Creates new user
 router.post('/signup', function (req, res) {
   // TODO : create a new user and sign them in
+  res.json({'success':true});
 });
 
 // Authenticates a user
 router.post('/signin', function (req, res) {
   // TODO : Authenticate user and create a session
+  res.json({'success':true});
 });
 
 // Get user info by id
 router.get('/:id', function (req, res) {
-
+  res.json({'success':true});
 });
 
 // Update user info by id
 router.put('/:id', function (req, res) {
-
+  res.json({'success':true});
 });
 
 // Get all projects for a user
 router.get('/:id/projects', function (req, res) {
-
+  res.json({'success':true});
 });
 
 // Get all collaborations for a user
 router.get('/:id/collaborations', function (req, res) {
-
+  res.json({'success':true});
 });
 
 // Get all recordings for a user
 router.get('/:id/recordings', function (req, res) {
-
+  res.json({'success':true});
 });
 
 // Get all lyrics for a user
 router.get('/:id/lyrics', function (req, res) {
-
+  res.json({'success':true});
 });
 
 module.exports = router;
