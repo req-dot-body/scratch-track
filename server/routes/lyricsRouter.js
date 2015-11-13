868c2eaf6faa615bcf524a20c27babe4a91ba964
Adam Ziehl <ajziehl@gmail.com>
2015-11-13 16:09:25 -0600

    Have lyric id readily available

--- server/routes/lyricsRouter.js
+++ server/routes/lyricsRouter.js
@@ -7,18 +7,21 @@ router.post('/', function (req, res) {
 });
 
 // Get a lyric by id
-router.get('/:id', function (req, res) {
-  res.json({'success':true});
+router.get('/:lyricId', function (req, res) {
+  var lyricId = req.params.lyricId;
+  res.json({'success':true,lyricId:lyricId});
 });
 
 // Update a lyric by id
-router.put('/:id', function (req, res) {
-  res.json({'success':true});
+router.put('/:lyricId', function (req, res) {
+  var lyricId = req.params.lyricId;
+  res.json({'success':true,lyricId:lyricId});
 });
 
 // Delete a lyric by id
-router.delete('/:id', function (req, res) {
-  res.json({'success':true});
+router.delete('/:lyricId', function (req, res) {
+  var lyricId = req.params.lyricId;
+  res.json({'success':true,lyricId:lyricId});
 });
 
 module.exports = router;
