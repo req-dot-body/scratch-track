3f4f6c89e52040603dd479a7ca117f5dca10d598
Adam Ziehl <ajziehl@gmail.com>
2015-11-13 12:28:41 -0600

    Add routes for users

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,8 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+// This wont be here eventually
+// Get all users (for testing)
+router.get('/', function (req, res) {
+  if (process.env.NODE_ENV === 'production') { // Just in case we accidentally forget to remove this handler
+    return;
+  }
   res.json({'success':true});
+});
+
+// Creates new user
+router.post('/signup', function (req, res) {
+  // TODO : create a new user and sign them in
+});
+
+// Authenticates a user
+router.post('/signin', function (req, res) {
+  // TODO : Authenticate user and create a session
+});
+
+// Get user info by id
+router.get('/:id', function (req, res) {
+
+});
+
+// Update user info by id
+router.put('/:id', function (req, res) {
+
+});
+
+// Get all projects for a user
+router.get('/:id/projects', function (req, res) {
+
+});
+
+// Get all collaborations for a user
+router.get('/:id/collaborations', function (req, res) {
+
+});
+
+// Get all recordings for a user
+router.get('/:id/recordings', function (req, res) {
+
+});
+
+// Get all lyrics for a user
+router.get('/:id/lyrics', function (req, res) {
+
 });
 
 module.exports = router;
