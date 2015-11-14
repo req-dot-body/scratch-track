bb84956c492d811967f9b00400f3765f31406d95
sca <smartinsantos@gmail.com>
2015-11-13 19:18:17 -0600

    added routes to the 3 states on lading public-signedin-signedup

---
+++ client/controllers/HomeCtrl.js
@@ -0,0 +1,19 @@
+app.controller('HomeCtrl', ['$scope','$state', function($scope,$state) {
+// the idea is to have a service that will give us 2 states loggedin / public that will change the states for what the user hits when it gets to the website
+
+//----CODE FOR TESTING THE CLIENT WHILE WE DON'T HAVE FUNCTIONALITY IMPLEMENTED-----
+
+  $scope.gotoPublic = function() {
+    console.log('gotopublic')
+    $state.go('home.public')
+  }
+  $scope.gotoSignedup = function() {
+    console.log('gotoSignedUp')
+    $state.go('home.signedup')
+  }
+  $scope.gotoLoggedin = function() {
+    console.log('gotologgedin')
+    $state.go('home.loggedin')
+
+  }
+}]);
--- client/controllers/MainCtrl.js
+++ client/controllers/MainCtrl.js
@@ -1,4 +1,4 @@
 app.controller('MainCtrl', ['$scope', function($scope) {
-  $scope.welcome = 'Welcome to your App!';
-  $scope.buttonText = 'This is your Button';
+  console.log('Main Controller hitted')
+
 }]);
