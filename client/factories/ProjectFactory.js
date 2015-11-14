c3ddd5e25c64ff8d75615ece26d67f3d6dc50971
NPoling <npoling@gmail.com>
2015-11-14 11:33:56 -0600

    Added edit button to project view.  Wrote out partial logic for Project factory to pull data from server and pass to view via controller.

--- client/factories/ProjectFactory.js
+++ client/factories/ProjectFactory.js
@@ -1,3 +1,13 @@
 app.factory('Project', ['$http', function($http) {
-  
+
+  var filepath = '/api/users' + userId;
+  var userId;
+  var projectDataFactory = {};
+
+  projectDataFactory.getProjects = function(){
+    return $http.get(filepath + '/projects');
+  } 
+
+
+  return projectDataFactory;
 }]);
