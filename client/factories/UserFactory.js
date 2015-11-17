85baaddf66976ae75c1b25e5d06a6e992580ad86
sca <smartinsantos@gmail.com>
2015-11-17 12:29:34 -0600

    user factory done

--- client/factories/UserFactory.js
+++ client/factories/UserFactory.js
@@ -4,30 +4,25 @@ app.factory('User', ['$http','$state', function($http,$state) {
   var logIn = function (userData){
 
     return $http.post('/api/users/signin', userData)
-    .then(function(response){
-      // console.log('logged in?', response);
-      if (response.status === 200) {
-        $state.go('home');
-        return true;
-      } else {
-        $state.go('public.signin');
-        return false;
-      }
+    .then(function(response){ 
+      $state.go('main.projects');
+    })
+    .catch(function(err) {
+      //display error message
+      $scope.signinError = 'Auth Error Please Re-Enter Info';
+      $state.go('public.signin');
     });
-
   };
 
   var signUp = function (newUser){
 
     return $http.post('/api/users/signup', newUser)
     .then(function(response){
-      // console.log('logged in?', response);
       if (response.status === 200) {
         $state.go('home');
         return true;
       } else {
         $state.go('public.signup');
-        console.log(response);
         return false;
       }
     });
