fb365d6a3f47c3e883fe27f11d190dd487ccb819
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-17 16:45:12 -0600

    basic view structure

--- client/controllers/RecordingCtrl.js
+++ client/controllers/RecordingCtrl.js
@@ -1,3 +1,48 @@
-app.controller('RecordingCtrl', ['$scope', 'Recording', function($scope, Recording) {
+app.controller('RecordingCtrl', ['$scope', '$state', 'Recording', 'Project',
+	function($scope, $state, Recording, Project) {
+
+	$(document).ready(function() {
+    $('.collapsible').collapsible({
+      accordion : false
+     });
+  });
+
+ $scope.mockData = [
+    {
+      project_id: 1,
+      url: 'www.mediaupload.com/test.mp3',
+      created_at: '10/6/15'
+    },
+    {
+      project_id: 1,
+      url: 'www.mediaupload.com/toadcroak.mp3',
+      description: 'sampled from a swamp in the dead of night',
+      created_at: '11/11/15'
+    },
+    {
+      project_id: 1,
+      url: 'www.mediaupload.com/frognado.mp3',
+      created_at: '11/12/15',
+      name: 'Frognado',
+      description: 'it was quite the blustery frognado'
+    }
+  ]
+
+	$scope.getAll = function(){
+		//$state.params should get me my id
+		//something from Project factory
+	}
+
+	$scope.add = function(){
+
+	}
+
+	$scope.edit = function(){
+
+	}
+
+	$scope.delete = function(){
+
+	}
 
 }]);
