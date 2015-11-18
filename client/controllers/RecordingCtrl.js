d9daa20cece1c20542ac055d5a5e05d748e990af
NPoling <npoling@gmail.com>
2015-11-18 16:42:33 -0600

    Rearranged HTML elements to match app style and removed superflous recording logs.

--- client/controllers/RecordingCtrl.js
+++ client/controllers/RecordingCtrl.js
@@ -48,6 +48,8 @@ app.controller('RecordingCtrl', ['$scope', '$state', 'Recording', 'Project',
 		Recording.del(id);
 	}
 
+	//RecorderJS:
+
 	$scope.audio_context;
   $scope.recorder;
 
@@ -59,8 +61,8 @@ app.controller('RecordingCtrl', ['$scope', '$state', 'Recording', 'Project',
       window.URL = window.URL || window.webkitURL;
       
       audio_context = new AudioContext;
-      __log('Audio context set up.');
-      __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+      //__log('Audio context set up.');
+      //__log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
     } catch (e) {
       alert('No web audio support in this browser!');
     }
