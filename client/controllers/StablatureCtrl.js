ff320271399681b10c0cdd4271a4c8b32ad3f257
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-18 19:21:42 -0600

    factory written

--- client/controllers/StablatureCtrl.js
+++ client/controllers/StablatureCtrl.js
@@ -1,3 +1,9 @@
 app.controller('StablatureCtrl', ['$scope', 'Stablature', function($scope, Stablature) {
 
+	$scope.code = "tabstave notation=false \n notes "+$scope.userInput;
+
+	$scope.submit = function(){
+		Stablature.submit($scope.code);
+	}
+
 }]);
--- client/factories/StablatureFactory.js
+++ client/factories/StablatureFactory.js
@@ -1,3 +1,22 @@
 app.factory('Stablature', ['$http', function($http) {
-  return {};
+
+
+  var create = function(stabInfo){
+  	return $http.post('api/stablature', stabInfo);
+  };
+
+  var edit = function(stabId, stabInfo){
+  	return $http.put('api/stablature/+'stabId, stabInfo);
+  };
+
+  var del = function(stabId){
+  	return $http.delete('api/stablature/+'stabId)
+  };
+
+  return {
+  	create: create,
+  	edit: edit,
+  	del: del
+  };
+
 }]);
