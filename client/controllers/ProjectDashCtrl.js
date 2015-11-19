eef109fffa95d89b08730a6c59e78cffa6b4dd8e
sca <smartinsantos@gmail.com>
2015-11-19 15:10:25 -0600

    added delete project to project dash

--- client/controllers/ProjectDashCtrl.js
+++ client/controllers/ProjectDashCtrl.js
@@ -1,5 +1,38 @@
 app.controller('ProjectDashCtrl', ['$scope','$state','Project', function($scope,$state,Project) {
 
+//load delete modal
+$('.modal-trigger').leanModal();
+
+$scope.deleteProject = function(id){
+
+  Project.deleteProject(id)
+  .then(function(){
+    $state.go('main.projects')
+  })
+};
+
+
+
+$scope.displayRecordings = function(recordings){
+
+};
+
+$scope.displayNotes = function(notes){
+
+};
+
+$scope.displayLyrics = function (lyrics) {
+
+};
+
+$scope.displayStablature = function (stablature) {
+
+};
+
+
+
 
 
 }]);
+
+
