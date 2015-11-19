8ff6923abf2f81229c1e9360e8a0d105292ae89a
Diandra Ryan-Mas <dryanmas@live.com>
2015-11-18 19:52:03 -0600

    tabs can be written and submitted

--- client/controllers/StablatureCtrl.js
+++ client/controllers/StablatureCtrl.js
@@ -1,9 +1,16 @@
-app.controller('StablatureCtrl', ['$scope', 'Stablature', function($scope, Stablature) {
+app.controller('StablatureCtrl', ['$scope', '$state', 'Stablature', 
+	function($scope, $state, Stablature) {
 
-	$scope.code = "tabstave notation=false \n notes "+$scope.userInput;
+	$scope.code = "tabstave notation=false \n notes 4-5/3";
 
-	$scope.submit = function(){
-		Stablature.submit($scope.code);
+	$scope.create = function(){
+		console.log('id?', $state.params.id)
+		var newStab = {
+			code: $scope.code,
+			project_id: $state.params.id
+		};
+
+		Stablature.create(newStab);
 	}
 
 }]);
--- client/factories/StablatureFactory.js
+++ client/factories/StablatureFactory.js
@@ -1,16 +1,24 @@
 app.factory('Stablature', ['$http', function($http) {
 
-
   var create = function(stabInfo){
-  	return $http.post('api/stablature', stabInfo);
+  	return $http.post('api/stablature', stabInfo)
+  	.catch(function(err){
+  		console.log('could not create new stab', err);
+  	})
   };
 
   var edit = function(stabId, stabInfo){
-  	return $http.put('api/stablature/+'stabId, stabInfo);
+  	return $http.put('api/stablature/'+stabId, stabInfo)
+  	.catch(function(err){
+  		console.log('could not updated stab', err);
+  	})
   };
 
   var del = function(stabId){
-  	return $http.delete('api/stablature/+'stabId)
+  	return $http.delete('api/stablature/'+stabId)
+  	.catch(function(err){
+  		console.log('could not delete stab', err);
+  	})
   };
 
   return {
