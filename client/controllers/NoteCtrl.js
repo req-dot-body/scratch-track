b603905e42ca81cd8f07d13e2625ae17cee2fdec
NPoling <npoling@gmail.com>
2015-11-21 14:29:49 -0600

    Re-instated note controller and factory methods

--- client/controllers/NoteCtrl.js
+++ client/controllers/NoteCtrl.js
@@ -1,3 +1,30 @@
-app.controller('NoteCtrl', ['$scope', 'Note', function($scope, Note) {
+app.controller('NoteCtrl', ['$scope', '$state', 'Note', 'Project', function($scope, $state, Note, Project) {
+
+  var projectId = $state.params.id;
+
+  $scope.notes = [{
+    name: 'test note',
+    text: 'ballsballsballsballsballsballsballsballs',
+    projectId: projectId
+  }]
+
+  $scope.newNote = {
+    name: '',
+    text: '',
+    project_id: projectId
+  }
+
+  $scope.getAll = function (projectId) {
+    Project.getProjectNotes(projectId);
+  }
+
+  $scope.addNote = function (newNote) {
+    Note.create(newNote);
+  }
+
+  $scope.deleteNote = function (noteId) {
+    Note.del(noteId);
+    $scope.getAll(projectId);
+  }
 
 }]);
