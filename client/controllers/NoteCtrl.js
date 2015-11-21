195806b9c0f2b59f4ead20c20f4dbc42fbe36b73
NPoling <npoling@gmail.com>
2015-11-21 16:26:19 -0600

    Minor updates.

--- client/controllers/NoteCtrl.js
+++ client/controllers/NoteCtrl.js
@@ -2,11 +2,23 @@ app.controller('NoteCtrl', ['$scope', '$state', 'Note', 'Project', function($sco
 
   var projectId = $state.params.id;
 
-  $scope.notes = [{
-    name: 'test note',
-    text: 'ballsballsballsballsballsballsballsballs',
-    projectId: projectId
-  }]
+  $scope.notes = [
+    {
+      name: 'test',
+      text: 'testetesetsetsetsetst',
+      created_at: '11/11/15'
+    },
+    {
+      name: 'test',
+      text: 'testetesetsetsetset112121st',
+      created_at: '11/11/15'
+    },
+    {
+      name: 'test',
+      text: 'testetesetsetsetsetst',
+      created_at: '11/11/15'
+    }
+  ];
 
   $scope.newNote = {
     name: '',
@@ -19,12 +31,18 @@ app.controller('NoteCtrl', ['$scope', '$state', 'Note', 'Project', function($sco
   }
 
   $scope.addNote = function (newNote) {
+    console.log(newNote);
     Note.create(newNote);
+    $scope.getAll(projectId);
   }
 
   $scope.deleteNote = function (noteId) {
     Note.del(noteId);
     $scope.getAll(projectId);
+  }
+
+  $scope.editNote = function (noteId) {
+
   }
 
 }]);
