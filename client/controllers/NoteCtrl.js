a198ff2439860861e1afc0923ec2d35e8a30d854
NPoling <npoling@gmail.com>
2015-11-30 16:06:30 -0600

    Sketched out notes view.  Integration with back-end is not complete, and accordion does not properly update on data change.

--- client/controllers/NoteCtrl.js
+++ client/controllers/NoteCtrl.js
@@ -6,17 +6,20 @@ app.controller('NoteCtrl', ['$scope', '$state', 'Note', 'Project', function($sco
     {
       name: 'test',
       text: 'testetesetsetsetsetst',
-      created_at: '11/11/15'
+      created_at: '11/11/15',
+      id: 1
     },
     {
       name: 'test',
       text: 'testetesetsetsetset112121st',
-      created_at: '11/11/15'
+      created_at: '11/11/15',
+      id: 2
     },
     {
       name: 'test',
       text: 'testetesetsetsetsetst',
-      created_at: '11/11/15'
+      created_at: '11/11/15',
+      id: 3
     }
   ];
 
@@ -26,23 +29,47 @@ app.controller('NoteCtrl', ['$scope', '$state', 'Note', 'Project', function($sco
     project_id: projectId
   }
 
+  $scope.noteEdits = {
+    name: '',
+    text: '',
+    project_id: projectId
+  }
+
+  //Note Methods:
   $scope.getAll = function (projectId) {
-    Project.getProjectNotes(projectId);
+    var notes = Project.getProjectNotes(projectId);
+    console.log(notes)
+    $scope.notes = notes.data
   }
 
   $scope.addNote = function (newNote) {
-    console.log(newNote);
-    Note.create(newNote);
-    $scope.getAll(projectId);
+    return Note.create(newNote).then = function (response) {
+      console.log("fn called")
+      $scope.getAll(projectId);
+    }
   }
 
   $scope.deleteNote = function (noteId) {
-    Note.del(noteId);
-    $scope.getAll(projectId);
+    return Note.del(noteId).then = function (response) {
+      $scope.getAll(projectId);
+    }
   }
 
-  $scope.editNote = function (noteId) {
+  $scope.editToggle = false;
 
+  $scope.editNote = function () {
+    $scope.editToggle = true;
+    var text = $('#note-text');
+    text.removeAttr('readonly');
+  }
+
+  $scope.confirmEdit = function (noteId, value) {
+    $scope.editToggle = false;
+    var text = $('#note-text');
+    text.attr('readonly', 'true ');
+    return Note.editBody(noteId, value).then = function (response) {
+      $scope.getAll(projectId);
+    }
   }
 
 }]);
--- client/factories/NoteFactory.js
+++ client/factories/NoteFactory.js
@@ -1,13 +1,12 @@
 app.factory('Note', ['$http', function($http) {
   
 var create = function(data) {
-    console.log('data:', data);
-    return $http.post('api/resources/notes', data);
-  }
+  console.log('data:', data);
+  return $http.post('api/resources/notes', data);
+}
 
-
-var edit = function(noteId) {
-
+var editBody = function(noteId, data) {
+  return $http.put('api/resources/notes/' + noteId, data);
 }
 
 var del = function(noteId) {
@@ -17,7 +16,7 @@ var del = function(noteId) {
 
 return {
   create: create,
-  edit: edit,
+  editBody: editBody,
   del: del
 }
   
