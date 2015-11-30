c13aab71a6afdf4569efdd0bd5184228bd52f9b4
sca <smartinsantos@gmail.com>
2015-11-30 17:11:06 -0600

    fixed a bug where moment was not being loaded properly, added a module to be able to filter objects

--- client/app.js
+++ client/app.js
@@ -7,6 +7,27 @@ var ngSanitize = require('angular-sanitize');
 var ngTouch = require('angular-touch');
 var uiRouter = require('angular-ui-router');
 window.foundationRoutes = [];//[{"name":"home","url":"/","path":"/../"}];
+
+angular.module('angular-toArrayFilter', [])
+
+.filter('toArray', function () {
+  return function (obj, addKey) {
+    if (!angular.isObject(obj)) return obj;
+    if ( addKey === false ) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    } else {
+      return Object.keys(obj).map(function (key) {
+        var value = obj[key];
+        return angular.isObject(value) ?
+          Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+          { $key: key, $value: value };
+      });
+    }
+  };
+});
+
 window.app = angular.module('myApp', [
   'ngAnimate',
   'ngCookies',
@@ -17,6 +38,7 @@ window.app = angular.module('myApp', [
   'ngSanitize',
   'ngResource',
   'foundation',
+  'angular-toArrayFilter',
   // 'foundation.dynamicRouting',
   // 'foundation.dynamicRouting.animations'
   // ngAnimate,
