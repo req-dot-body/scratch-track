3ad236cbc1b69e312fd9a3d8f42855127ab33cea
Diandra Ryan-Mas <Diandra@Haydees-MacBook-Air.local>
2015-12-07 19:57:13 -0600

    removed hardcoded information

--- client/controllers/ProjectDashCtrl.js
+++ client/controllers/ProjectDashCtrl.js
@@ -5,45 +5,6 @@ app.controller('ProjectDashCtrl', ['$scope','$state','Project', 'FoundationApi',
 
 projectId = $state.params.id; 
 
-$scope.testRecording = {
-  name: "a recording",
-  description: "that's pretty much all there is to it",
-  url: "https://scratch-track.s3.amazonaws.com/recordings/a39c9438-00e2-4f44-9261-36044cce02fb.wav"
-}
-
-$scope.testLyrics = { 
-  text: 'Darkness at the break of noon'+
-'\nShadows even the silver spoon'+
-'\nThe handmade blade, the child\'s balloon'+
-'\nEclipses both the sun and moon' +
-'\nTo understand you know too soon' +
-'\nThere is no sense in trying.'+
-'\n '+
-'\nPointed threats, they bluff with scorn'+
-'\nSuicide remarks are torn'+
-'\nFrom the fools gold mouthpiece'+
-'\nThe hollow horn plays wasted words'+
-'\nProved to warn'+
-'\nThat he not busy being born'+
-'\nIs busy dying.'+
-'\n'+
-'\nTemptation\'s page flies out the door'+
-'\nYou follow, find yourself at war'+
-'\nWatch waterfalls of pity roar'+
-'\nYou feel to moan but unlike before'+
-'\nYou discover'+
-'\nThat you\'d just be'+
-'\nOne more person crying.'+
-'\n'+
-'\nSo don\'t fear if you hear'+
-'\nA foreign sound to you ear'+
-'\nIt\'s alright, Ma, I\'m only sighing.',
-  created_at: 150817546,
-  name: 'It\'s Alright Ma',
-  description: 'a little something I came up with'    
-};
-
-
 Project.getProject(projectId)
 .then(function(response){
   $scope.projectData = response.data;
