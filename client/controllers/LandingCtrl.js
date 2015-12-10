4330693878f1b6f2fa205b57107dcc1230c75310
sca <smartinsantos@gmail.com>
2015-12-10 12:34:53 -0600

    landing public reestructure

--- client/controllers/LandingCtrl.js
+++ client/controllers/LandingCtrl.js
@@ -1,3 +1,3 @@
 app.controller('LandingCtrl', ['$scope', function($scope) {
-  $scope.message = "manage your songwriting workflow on the go";
+  $scope.message = "Enough napkings have been wasted!, Song writing as it should";
 }]);
