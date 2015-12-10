2a3b4bb4ca7b0fdf63618c0e17583a32cce3b64b
sca <smartinsantos@gmail.com>
2015-12-10 14:38:18 -0600

    misspelling fixed

--- client/controllers/LandingCtrl.js
+++ client/controllers/LandingCtrl.js
@@ -1,3 +1,3 @@
 app.controller('LandingCtrl', ['$scope', function($scope) {
-  $scope.message = "Enough napkings have been wasted!, Song writing as it should";
+  $scope.message = "Enough napkins have been wasted! Song writing as it should";
 }]);
