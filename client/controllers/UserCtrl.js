b0e4877e0c468eb98da7943041352fb504ade292
Adam Ziehl <ajziehl@gmail.com>
2015-12-11 18:21:40 -0600

    Display error message when using invalid details to log in. Resolves #381. Also found and fixed a bug with sign in, Resolves #508

--- client/controllers/UserCtrl.js
+++ client/controllers/UserCtrl.js
@@ -1,20 +1,25 @@
 app.controller('UserCtrl', ['$scope','User', function($scope, User) {
   
-  $scope.email = '',
-  $scope.password = '',
-  $scope.first ='',
-  $scope.last ='',
+  $scope.email = '';
+  $scope.password = '';
+  $scope.first ='';
+  $scope.last ='';
+  $scope.error = { message: '' };
 
   $scope.signinUser = function () {
+    $scope.error.message = '';
     var userData = {};
     userData.email = $scope.email;
     userData.password = $scope.password;
-    console.log(userData.email,' ',userData.password)
     //send JSON object to server via factory call
-    User.logIn(userData);
-  }
+    User.logIn(userData).catch(function (err) {
+      console.log('Error with signin:', err);
+      $scope.error.message = 'Invalid details provided';
+    });
+  };
   
   $scope.signupUser = function () {
+    $scope.error.message = '';
     var newUser = {};
     newUser.email = $scope.email;
     newUser.password = $scope.password;
@@ -27,8 +32,13 @@ app.controller('UserCtrl', ['$scope','User', function($scope, User) {
       userData.email = $scope.email;
       userData.password = $scope.password;
       //sends true to identify that user just signedup
+      console.log('User signed up, logging them in')
       User.logIn(userData);
     })
-  } 
+    .catch((err) => {
+      $scope.error.message = 'Error signing up, please try again';
+      console.log('Signup error:', err);
+    });
+  } ;
 
 }]);
--- client/factories/UserFactory.js
+++ client/factories/UserFactory.js
@@ -1,33 +1,36 @@
-app.factory('User', ['$http', '$state', 'signedUp', function($http, $state, signedUp) {
+app.factory('User', ['$http', '$state', '$timeout', 'signedUp', function($http, $state, $timeout, signedUp) {
   
   var logIn = function (userData){
 
     return $http.post('/api/users/signin', userData)
-    .then(function(response){ 
-      $state.go('main.projects');
-    })
-    .catch(function(err) {
-      //display error message
-      $state.go('public.signin');
-      return 'Auth Error Please Re-Enter Info';
+    .then(function (response) {
+      $timeout(() => {
+        $state.go('main.projects', null, { reload: true });
+      }, 100);
     });
+    // .catch(function(err) {
+    //   //display error message
+    //   console.log('Error signing in:', err);
+    //   $state.go('public.signin');
+    //   return 'Auth Error Please Re-Enter Info';
+    // });
   };
 
   var signUp = function (newUser){
     //if user is signed up sends a parameter true to load the tour
     signedUp.value = true;
-    return $http.post('/api/users/signup', newUser)
-    .catch(function(err){
-      $state.go('public.signup');
-      console.log('signUp err: ', err);
-      return 'Auth Error Please Re-Enter Info';
-    });
+    return $http.post('/api/users/signup', newUser);
+    // .catch(function(err){
+    //   $state.go('public.signup');
+    //   console.log('signUp err: ', err);
+    //   return 'Auth Error Please Re-Enter Info';
+    // });
   };
 
   var logOut = function(){
     return $http.post('/api/users/signout')
     .then(function(response){
-        $state.go('home');
+      $state.go('home');
     })
     .catch(function(err){  
       console.log('logOut err ', err);
